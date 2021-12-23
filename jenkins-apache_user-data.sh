9a719407d633fd1a4e802ba3dfd8307f73b4bef6
Maxim Biloborodko <berzerker994@gmail.com>
2021-12-23 17:21:24 +0200

    4

--- jenkins-apache_user-data.sh
+++ jenkins-apache_user-data.sh
@@ -2,7 +2,7 @@
 echo "---------start-------------"
 apt -y update
 apt -y install apache2
-apt -y install openjdk-8-jre
+apt -y install openjdk-11-jre
 s=$(wget -qO- eth0.me)
 echo "<html><body><br><br><br><center><a href="http://$s:1234">Jenkins</a></center><br><br><br><center></center></body></html>" > /var/www/html/index.html
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
