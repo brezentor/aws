b940863f8a7f3f4da64c8bfda659023ef228593f
Maxim Biloborodko <berzerker994@gmail.com>
2022-01-24 17:16:50 +0200

    5

--- jenkins-python-docker-apache_UD.sh
+++ jenkins-python-docker-apache_UD.sh
@@ -11,4 +11,12 @@ apt -y update
 apt -y install jenkins
 sed -i "s/HTTP_PORT=8080/HTTP_PORT=1234/g" /etc/default/jenkins
 service jenkins restart
+apt -y update
+apt -y install python
+apt -y update
+apt -y install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt -y update
+apt -y install docker-ce docker-ce-cli containerd.io
 echo "---------end-------------"
