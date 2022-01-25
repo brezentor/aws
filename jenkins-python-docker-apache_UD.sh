32c5f5f97b1c1ddb22a7a5b506145b85a619dfe8
Maxim Biloborodko <berzerker994@gmail.com>
2022-01-25 09:15:58 +0200

    7

--- jenkins-python-docker-apache_UD.sh
+++ jenkins-python-docker-apache_UD.sh
@@ -19,7 +19,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt -y update
 apt -y install docker-ce docker-ce-cli containerd.io
-usermod -aG jenkins sudo
+usermod -aG sudo jenkins
 touch /etc/sudoers.d/jenkins-sudo
 echo "jenkins ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/jenkins-sudo
 echo "---------end-------------"
